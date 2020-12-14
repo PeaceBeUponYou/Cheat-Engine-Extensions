1757f9938c098fbc610af5214a1fcdb69ce33713
PeaceBeUponYou <70230395+PeaceBeUponYou@users.noreply.github.com>
2020-12-15 00:05:47 +0500

    Add files via upload

---
+++ Address From Pointers.lua
@@ -0,0 +1,49 @@
+------------######  Author--------------PeaceBeUponYou
+------------######  Community-----------Cheat The Game
+------------######			:Join US:
+------------######	Discord:  https://discordapp.com/invite/ndn4pqs
+------------###### 	MY GitHub: https://github.com/PeaceBeUponYou/Cheat-Engine-Extensions
+------------######  Ext Name: Copy Address from A pointer
+------------######  Ext Version: 0.1.1
+-------------------------------------------------------
+--creating item in PopupMenu2
+mm = MainForm.PopupMenu2
+local crtItem = createMenuItem(mm.Items)
+crtItem.Name = "copyAddress1"
+crtItem.Caption = "Copy Selected Address"
+crtItem.Shortcut = "CTRL+ALT+C"
+mm.Items.Insert(1,crtItem)
+
+local crtItem2 = createMenuItem(mm.Items)
+crtItem2.Name = "copyAddress2"
+crtItem2.Caption = "Add as an Address"
+mm.Items.Insert(2,crtItem2)
+
+--copy address to clipboard
+adl = getAddressList()
+crtItem.OnClick = function()
+	sel = adl.SelectedRecord
+	if not sel then showMessage("Invalid Address"); return end;
+	if not (sel.Address) then showMessage("Invalid Address"); return end;
+	if sel.CachedAddress then
+		writeToClipboard(sel.CachedAddress)
+	else
+		writeToClipboard(sel.Address)
+	end
+end
+
+--CopyAddress from Pointer and paste as single address
+crtItem2.OnClick = function()
+	sel = adl.SelectedRecord
+	if not sel then showMessage("Invalid Address"); return end;
+	if not (sel.Address) then showMessage("Invalid Address"); return end;
+	if sel.CachedAddress then
+		crtRec = adl.createMemoryRecord()
+		crtRec.Address = sel.CachedAddress
+		crtRec.Description = "PointedAddress"
+	else
+		crtRec = adl.createMemoryRecord()
+		crtRec.Address = sel.Address
+		crtRec.Description = "PointedAddress"
+	end
+end
